10f9fdae69bf66cf5898cfd2d8355665372bf788
Vladimir <farvoz@mail.ru>
2025-09-21 20:26:53 +0300

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import Game from './components/Game';
 
 // Create React root and render the app
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(container!);
 root.render(<Game  />); 
